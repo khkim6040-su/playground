0efb792901e7b44d0155065e6bbe095560e3bf9c
Gwanho Kim <khkim6040@shiftup.co.kr>
2025-08-04 21:23:11 +0900

    global arg에 인자 설정

--- .github/actions/slack/message.py
+++ .github/actions/slack/message.py
@@ -6,15 +6,19 @@ class GlobalArgs:
     pretty: bool
     repository: str
     test: bool
+    channel_id: str | None
+    thread_ts: str | None
 
-    def __init__(self, pretty: bool, repository: str, test: bool):
+    def __init__(self, pretty: bool, repository: str, test: bool, channel_id: str | None, thread_ts: str | None):
         self.pretty = pretty
         self.repository = repository
         self.test = test
+        self.channel_id = channel_id
+        self.thread_ts = thread_ts
 
     @staticmethod
     def from_args(args):
-        return GlobalArgs(args.pretty, args.repository, args.test)
+        return GlobalArgs(args.pretty, args.repository, args.test, args.channel_id, args.thread_ts)
 
     @staticmethod
     def build_parser(parser):
@@ -23,6 +27,10 @@ class GlobalArgs:
         )
         parser.add_argument("--repository", required=True, help="GitHub 레포지토리")
         parser.add_argument("--test", action="store_true", help="테스트용 메시지")
+        parser.add_argument("--channel-id", required=False, help="메시지를 보낼 Slack 채널 ID")
+        parser.add_argument(
+            "--thread-ts", required=False, help="스레드 댓글을 달 메세지의 타임스탬프. channel_id와 함께 사용되어야 함"
+        )
 
 
 class MsgArg(ABC):
